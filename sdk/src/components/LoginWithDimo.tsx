27db309769b602508cc63b164f206379e74b644c
moizahmedd <ahmedmoiz854@gmail.com>
2024-10-25 21:07:57 -0400

    add iframe id, and allow passkey create/get

--- sdk/src/components/LoginWithDimo.tsx
+++ sdk/src/components/LoginWithDimo.tsx
@@ -38,20 +38,21 @@ const LoginWithDimo: React.FC<LoginWithDimoProps> = ({
   // Trigger embedAuth only once the iframe has fully loaded
   const handleIframeLoad = () => {
     if (mode === "embed") {
-      embedAuth(onSuccess, onError);
+      embedAuth(onSuccess, onError, clientId, redirectUri, apiKey);
     }
   };
 
   // Renders iframe for embed mode
   const renderEmbedIframe = () => (
     <iframe
-      src="http://localhost:3000" // Pull Dimo Login URL from ENV
+      id="dimo-iframe"
+      src="https://ab1a735dff55.ngrok.app/" // Pull Dimo Login URL from ENV
+      allow="publickey-credentials-create; publickey-credentials-get" //TODO: Limit this to specific trusted origins
       width="100%"
       height="250px" //Allow this to be customized by developer
       title="Dimo Login"
       style={{ border: "4px solid white" }}
       frameBorder="0"
-      allow="fullscreen"
       onLoad={handleIframeLoad} // Trigger embedAuth when the iframe loads
     />
   );
