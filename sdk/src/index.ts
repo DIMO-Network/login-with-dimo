2273353cbf318310523b2d1ee6c1d276cecc017a
moizahmedd <ahmedmoiz854@gmail.com>
2024-11-18 18:29:56 -0500

    update imports, and sample app with usage

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,1 +1,9 @@
+// Export components
 export { default as LoginWithDimo } from './components/LoginWithDimo';
+export { default as ShareVehiclesWithDimo } from './components/ShareVehiclesWithDimo';
+
+// Export config initialization
+export { initializeDimoSDK, getDimoConfig } from './config/sdkConfig';
+
+// Export enums
+export { EntryState } from './enums/globalEnums';
