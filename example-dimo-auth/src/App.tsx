69926dc8e21c6e3bfe38b0be20d082d3b3e229e1
Neil Devas <neil.devas97@gmail.com>
2025-04-08 13:02:53 -0400

    all updates

--- example-dimo-auth/src/App.tsx
+++ example-dimo-auth/src/App.tsx
@@ -9,13 +9,11 @@ import {
 } from '@dimo-network/login-with-dimo';
 import { sampleAbi } from './abi/sample-abi';
 
+const sampleExpirationDate = new Date(Date.UTC(2025, 11, 11, 18, 51)); // Note: Month is zero-based
+
 function App() {
   const [permissionsEnabled, setPermissionsEnabled] = useState(false);
   const [forceEmail, setForceEmail] = useState(false);
-  const { isAuthenticated, email, walletAddress } = useDimoAuthState();
-
-  const sampleExpirationDate = new Date(Date.UTC(2025, 11, 11, 18, 51)); // Note: Month is zero-based
-
   initializeDimoSDK({
     clientId: process.env.REACT_APP_DIMO_CLIENT_ID!,
     redirectUri: process.env.REACT_APP_DIMO_REDIRECT_URI!,
@@ -56,100 +54,102 @@ function App() {
             Force Email
           </label>
         </div>
-
-        {isAuthenticated && (
-          <div>
-            <p>Connected User</p>
-            <p>Wallet Address:{walletAddress}</p>
-            {email && <p>{email}</p>}
-          </div>
-        )}
-
-        <div>
-          <h3>Popup Example</h3>
-
-          <LoginWithDimo
-            mode="popup"
-            onSuccess={(authData: any) => console.log('Success:', authData)}
-            onError={(error: any) => console.error('Error:', error)}
-            permissionTemplateId={permissionsEnabled ? '2' : undefined}
-            unAuthenticatedLabel="Sign In with DIMO"
-            utm="MOIZ"
-            altTitle={true}
-            // Optionally, specify vehicles (uncomment the line below to use it)
-            // vehicles={["752", "742", "738", "722"]}
-            // vehicles={["585","586"]}
-          />
-
-          {isAuthenticated && (
-            <>
-              <ShareVehiclesWithDimo
-                mode="popup"
-                onSuccess={(authData: any) => console.log('Success:', authData)}
-                onError={(error: any) => console.error('Error:', error)}
-                permissionTemplateId={'2'}
-                onboarding={[]}
-              />
-
-              <ExecuteAdvancedTransactionWithDimo
-                mode="popup"
-                onSuccess={(transactionData: any) => {
-                  console.log(transactionData);
-                  console.log(
-                    'Transaction Hash:',
-                    transactionData.transactionHash
-                  );
-                }}
-                onError={(error: any) => console.error('Error:', error)}
-                address="0x21cFE003997fB7c2B3cfe5cf71e7833B7B2eCe10"
-                abi={sampleAbi}
-                functionName="transfer"
-                args={['0x62b98e019e0d3e4A1Ad8C786202e09017Bd995e1', '0']}
-              />
-            </>
-          )}
-        </div>
-
-        <div>
-          <h3>Redirect Example</h3>
-          <LoginWithDimo
-            mode="redirect"
-            onSuccess={(authData: any) => console.log('Success:', authData)}
-            onError={(error: any) => console.error('Error:', error)}
-            permissionTemplateId={permissionsEnabled ? '1' : undefined}
-            utm="dimo"
-          />
-
-          {isAuthenticated && (
-            <ShareVehiclesWithDimo
-              mode="redirect"
-              onSuccess={(authData: any) => console.log('Success:', authData)}
-              onError={(error: any) => console.error('Error:', error)}
-              permissionTemplateId={'2'}
-              expirationDate={sampleExpirationDate.toISOString()}
-              vehicles={['752', '742']}
-              onboarding={["tesla"]}
-            />
-          )}
-
-          <ExecuteAdvancedTransactionWithDimo
-            mode="redirect"
-            onSuccess={(transactionData: any) =>
-              console.log('Transaction Hash:', transactionData.transactionHash)
-            }
-            onError={(error: any) => console.error('Error:', error)}
-            address="0x21cFE003997fB7c2B3cfe5cf71e7833B7B2eCe10"
-            value="0"
-            abi={sampleAbi}
-            functionName="transfer"
-            args={['0x62b98e019e0d3e4A1Ad8C786202e09017Bd995e1', '0']}
-            authenticatedLabel="Execute Transaction"
-            unAuthenticatedLabel="Sign In to Execute Transaction with DIMO"
-          />
-        </div>
+        <UserData />
+        <Examples loginType={'popup'} />
+        <Examples loginType={'redirect'} />
       </header>
     </div>
   );
+}
+
+interface Props {
+  loginType: 'popup' | 'redirect',
+  permissionsEnabled?: string;
+}
+
+const UserData = () => {
+  const { isAuthenticated, email, walletAddress } = useDimoAuthState();
+  if (!isAuthenticated) return null;
+  return (
+    <div>
+      <p>Connected User</p>
+      <p>Wallet Address:{walletAddress}</p>
+      {!!email && <p>{email}</p>}
+    </div>
+  )
+}
+
+const Examples = (props: Props) => {
+  const {loginType, permissionsEnabled} = props;
+  const onSuccess = (data: unknown) => console.log('Success:', data);
+  const onError = (error: unknown) => console.log('Error:', error);
+  const { isAuthenticated } = useDimoAuthState();
+
+  return (
+    <div>
+      <h3>{loginType === 'popup' ? 'Popup' : 'Redirect'} Examples</h3>
+      <LoginWithDimo
+        mode={loginType}
+        onSuccess={onSuccess}
+        onError={onError}
+        permissionTemplateId={permissionsEnabled ? '1' : undefined}
+        utm="dimo"
+      />
+      {isAuthenticated && (
+        <>
+          <ShareVehiclesWithDimo
+            mode={loginType}
+            onSuccess={onSuccess}
+            onError={onError}
+            permissionTemplateId={'2'}
+            expirationDate={sampleExpirationDate.toISOString()}
+          />
+          <ShareVehiclesWithDimo
+            mode={loginType}
+            onSuccess={onSuccess}
+            onError={onError}
+            authenticatedLabel={'Connect a Tesla'}
+            unauthenticatedLabel={'Connect a Tesla'}
+            permissionTemplateId={'2'}
+            onboarding={['tesla']}
+          />
+          <ShareVehiclesWithDimo
+            mode={loginType}
+            onSuccess={onSuccess}
+            onError={onError}
+            authenticatedLabel={'Connect to ICE vehicles only'}
+            unauthenticatedLabel={'Connect to ICE vehicles only'}
+            permissionTemplateId={'2'}
+            powertrainTypes={['ICE']}
+          />
+          <AdvancedTransactionButton loginType={loginType} />
+        </>
+
+      )}
+    </div>
+  )
+}
+
+const AdvancedTransactionButton = (props: Pick<Props, 'loginType'>) => {
+  const onSuccess = (data: any) => {
+    console.log(data);
+    console.log(
+      'Transaction Hash:',
+      data.transactionHash,
+    );
+  }
+  const onError = (error: unknown) => console.error('Error:', error)
+  return (
+    <ExecuteAdvancedTransactionWithDimo
+      mode={props.loginType}
+      onSuccess={onSuccess}
+      onError={onError}
+      address="0x21cFE003997fB7c2B3cfe5cf71e7833B7B2eCe10"
+      abi={sampleAbi}
+      functionName="transfer"
+      args={['0x62b98e019e0d3e4A1Ad8C786202e09017Bd995e1', '0']}
+    />
+  )
 }
 
 export default App;
